bdb7e260701943d4443df2a29eb8c0efe00ab1ac
vishnu_suderson <122167792+vishnu-suderson@users.noreply.github.com>
2023-04-05 08:00:43 +0530

    Create Money count.c

---
+++ day1/Money count.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+#include <stdlib.h>
+int main()
+int a[8]=(500, 100,50,20,10,5,2,1), m, temp, i; 
+printf("Enter the Amount: ");
+scanf("%d", &m) ;
+temp-m;
+for (i=0; i<8;i++){
+printf("\nd is: %d", a[i], temp/a[i]);
+temp-tempsa[i];}
+return 0;
+}
